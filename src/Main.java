6df6f81ba6c9aae955d2e22807aeafb1d15bf40b
б а <bascetbol333@gmail.com>
2023-05-18 15:30:50 +0300

    d

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner scanner = new Scanner(new InputStreamReader(System.in));
+    private static int numSys = 10;
     public static void main(String[] args) {
         int menu = -1;
 
@@ -13,9 +14,9 @@ public class Main {
             System.out.println("2 - Вычитание");
             System.out.println("3 - Умножение");
             System.out.println("4 - Деление");
-            System.out.println("5 - Возвести в степень");
-            System.out.println("6 - Вычислить квадратный корень");
-            System.out.println("7 - Перевести в другую систему счисления");
+            //System.out.println("5 - Возвести в степень");
+            //System.out.println("6 - Вычислить квадратный корень");
+            System.out.println("7 - Изменить систему счисления");
             System.out.println("0 - Выход");
             String str = scanner.next();
             try {
@@ -27,21 +28,22 @@ public class Main {
             if(menu==2) sOperation(2);
             if(menu==3) sOperation(3);
             if(menu==4) sOperation(4);
-            if(menu==5) pow();
-            if(menu==6) sqrt();
-            if(menu==7) numSys();
+            //if(menu==5) pow();
+            //if(menu==6) sqrt();
+            if(menu==7) changeSys();
+            menu = -1;
         }
 
     }
 
     private static void sOperation(int op) {
         double result = 0;
-        double num1, num2;
+        String num1, num2;
         System.out.println("Введите первое число: ");
-        num1 = setNumDouble();
+        num1 = setNumString();
 
         System.out.println("Введите второе число: ");
-        num2 = setNumDouble();
+        num2 = setNumString();
 
         switch (op) {
             case 1: result = num1+num2; break;
@@ -105,24 +107,47 @@ public class Main {
         System.out.println("В шестнадцатеричной системе счисления: "+ Long.toString(num, 16));
     }
 
-    private static double setNumDouble() {
+
+    private static void changeSys() {
+        System.out.println("Текущая система счисления - " + numSys);
+        int num = 0;
+        System.out.println("Выберите систему счисления (2,8,10,16): ");
+        do{
+            num = setNumInt();
+        }while(num != 2 && num != 8 && num != 10 && num != 16);
+        numSys = num;
+        System.out.println("Система счисления изменена на " + numSys);
+    }
+
+    private static int setNumString() {
         String str;
-        while(true) {
+        int num = 0;
+        Long check = 0L;
+        Boolean f = false;
+        while(!f) {
             str = scanner.next();
             try {
-                return Double.parseDouble(str);
+                num = Integer.parseInt(str);
+                check = Long.parseLong(str, numSys);
+                //if(num != check) {
+                //    System.out.println("Число не в "+numSys+" системе счисления");
+               // }else
+                f = true;
             } catch (Exception e) {
                 System.out.println("Введено неправильное значение");
             }
         }
+        return num;
     }
 
     private static int setNumInt() {
         String str;
+        int num = 0;
         while(true) {
             str = scanner.next();
             try {
-                return Integer.parseInt(str);
+                num = Integer.parseInt(str);
+                return num;
             } catch (Exception e) {
                 System.out.println("Введено неправильное значение");
             }
